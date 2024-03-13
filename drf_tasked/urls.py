fb97008f01231c5e2aa8782e1ec4d2e74f3df72c
Aoife Kirby <aoifekirby33@gmail.com>
2024-03-13 22:52:46 +0000

    Add root route & welcome message

--- drf_tasked/urls.py
+++ drf_tasked/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import root_route
 
 urlpatterns = [
+    path('', root_route),
     path('admin/', admin.site.urls),
 ]
