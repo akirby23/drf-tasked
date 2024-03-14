2a31d2f10770447b5cc0c33c1564605ab3d4841a
Aoife Kirby <aoifekirby33@gmail.com>
2024-03-14 23:46:22 +0000

    Create ProfileList & ProfileDetail views, wire up URLs, add ProfileSerializer

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
+from drf_tasked.permissions import IsOwnerOrReadOnly
+from .models import Profile
+from .serializers import ProfileSerializer
 
-# Create your views here.
+# The models below have been adapted from Code Institute's drf-api walkthrough
+
+class ProfileList(generics.ListAPIView):
+    """
+    Retrieves and lists all profiles
+    """
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Read or update a profile if you're the owner
+    Read task details to be added later
+    """
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
