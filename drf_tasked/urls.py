d69eec6b8b00260d77a5b4c7b15afe94b3932408
Aoife Kirby <aoifekirby33@gmail.com>
2024-03-15 15:48:47 +0000

    Configure API authentication

--- drf_tasked/urls.py
+++ drf_tasked/urls.py
@@ -20,5 +20,6 @@ from .views import root_route
 urlpatterns = [
     path('', root_route),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('profiles.urls')),
 ]
