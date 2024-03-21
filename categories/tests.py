f5609c8fe8b50f32c95e6e9186958bd89edf6cb3
Aoife Kirby <aoifekirby33@gmail.com>
2024-03-21 20:31:19 +0000

    Add categories & prioritylevels tests

--- categories/tests.py
+++ categories/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from .models import Category
 
-# Create your tests here.
+class CategoryListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_superuser(username='admin', password='q5vwm5pv')
+        User.objects.create_user(username='user', password='1mn6ah5d')
+
+    def test_superuser_can_create_category(self):
+        self.client.login(username='admin', password='q5vwm5pv')
+        response = self.client.post('/categories/', {'name': 'Social', 'description':'Social'})
+        count = Category.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_non_superuser_cannot_create_category(self):
+        self.client.login(username='user', password='1mn6ah5d')
+        response = self.client.post('/categories/', {'name': 'Social', 'description':'Social'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_can_retrieve_category_list(self):
+        response = self.client.get('/categories/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class CategoryDetailViewTests(APITestCase):
+    def setUp(self):
+        admin = User.objects.create_superuser(username='admin', password='q5vwm5pv')
+        user = User.objects.create_user(username='user', password='1mn6ah5d')
+        category1 = Category.objects.create(name="Work", description="Work")
+        category2 = Category.objects.create(name="Education", description="Education")
+
+    def test_can_retrieve_category_with_valid_id(self):
+        response = self.client.get('/categories/1/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cannot_retrieve_category_with_invalid_id(self):
+        response = self.client.get('/categories/3/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_superuser_can_update_category(self):
+        self.client.login(username='admin', password='q5vwm5pv')
+        response = self.client.put('/categories/1/', {'description': 'Edited category'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_non_superuser_cannot_update_category(self):
+        self.client.login(username='user', password='1mn6ah5d')
+        response = self.client.put('/categories/1/', {'name':'Edited category'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
--- prioritylevels/tests.py
+++ prioritylevels/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from .models import PriorityLevel
 
-# Create your tests here.
+class PriorityLevelListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_superuser(username='admin', password='q5vwm5pv')
+        User.objects.create_user(username='user', password='1mn6ah5d')
+
+    def test_superuser_can_create_priority_level(self):
+        self.client.login(username='admin', password='q5vwm5pv')
+        response = self.client.post('/priority-levels/', {'name': 'Low', 'description':'Low'})
+        count = PriorityLevel.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_non_superuser_cannot_create_priority_level(self):
+        self.client.login(username='user', password='1mn6ah5d')
+        response = self.client.post('/priority-levels/', {'name': 'Low', 'description':'Low'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_can_retrieve_priority_level_list(self):
+        response = self.client.get('/priority-levels/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class PriorityLevelDetailViewTests(APITestCase):
+    def setUp(self):
+        admin = User.objects.create_superuser(username='admin', password='q5vwm5pv')
+        user = User.objects.create_user(username='user', password='1mn6ah5d')
+        prioritylevel1 = PriorityLevel.objects.create(name="Low", description="Low")
+        prioritylevel2 = PriorityLevel.objects.create(name="High", description="High")
+
+    def test_can_retrieve_priority_level_with_valid_id(self):
+        response = self.client.get('/priority-levels/1/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cannot_retrieve_priority_level_with_invalid_id(self):
+        response = self.client.get('/priority-levels/3/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_superuser_can_update_priority_level(self):
+        self.client.login(username='admin', password='q5vwm5pv')
+        response = self.client.put('/priority-levels/1/', {'name':'Edited priority level'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_non_superuser_cannot_update_priority_level(self):
+        self.client.login(username='user', password='1mn6ah5d')
+        response = self.client.put('/priority-levels/1/', {'name':'Edited priority level'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
