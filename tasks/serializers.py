cc4d76201684871a8d5834eb8f7c7299802eb3c0
Aoife Kirby <aoifekirby33@gmail.com>
2024-03-31 20:56:27 +0000

    Add category, priority level & assignee names as serializer fields

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -6,6 +6,9 @@ class TaskSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.profile_picture.url')
+    category_name = serializers.ReadOnlyField(source='category.name')
+    priority_level_name = serializers.ReadOnlyField(source='priority_level.name')
+    assignee_name = serializers.ReadOnlyField(source='assignee.username')
     comments_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
@@ -15,6 +18,7 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
             model = Task
             fields = [
-                'id', 'owner', 'is_owner', 'profile_id', 'profile_image', 'title', 'created_on', 'updated_on', 'category', 
+                'id', 'owner', 'is_owner', 'profile_id', 'profile_image', 'category_name', 
+                'priority_level_name', 'assignee_name', 'title', 'created_on', 'updated_on', 'category', 
                 'priority_level', 'status', 'task_detail', 'assignee', 'comments_count',
             ]
