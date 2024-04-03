b47407587832022502e7ffada651d2be30576d0a
Aoife Kirby <aoifekirby33@gmail.com>
2024-04-03 16:07:11 +0000

    Add filterset field to filter comments associated with tasks

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,6 @@
 from drf_tasked.permissions import IsOwnerOrReadOnly
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import CommentSerializer
 from .models import Comment
 
@@ -7,6 +8,8 @@ class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['task']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
