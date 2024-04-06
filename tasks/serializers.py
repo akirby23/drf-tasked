e7ed2ef09b5502381e94b5f79a11b0add75ccd97
Aoife Kirby <aoifekirby33@gmail.com>
2024-04-06 16:55:14 +0000

    Add status_name SerializerMethodField to return the human readable task status name

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Task
+from .models import TASK_STATUS
 
 class TaskSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
@@ -9,6 +10,7 @@ class TaskSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(source='owner.profile.profile_picture.url')
     category_name = serializers.ReadOnlyField(source='category.name')
     priority_level_name = serializers.ReadOnlyField(source='priority_level.name')
+    status_name = serializers.SerializerMethodField()
     assignee_name = serializers.ReadOnlyField(source='assignee.username')
     comments_count = serializers.ReadOnlyField()
 
@@ -19,12 +21,16 @@ class TaskSerializer(serializers.ModelSerializer):
     def get_is_assignee(self, obj):
         request = self.context['request']
         return request.user == obj.assignee
+    
+    def get_status_name(self, obj):
+        status_name = obj.get_status_display()
+        return status_name
 
     class Meta:
             model = Task
             fields = [
-                'id', 'owner', 'is_owner', 'is_assignee', 'profile_id', 'profile_image', 
-                'category_name', 'priority_level_name', 'assignee_name', 'title', 'created_on', 
-                'updated_on', 'category', 'priority_level', 'status', 'task_detail', 'assignee', 
-                'comments_count',
+                'id', 'owner', 'is_owner', 'is_assignee', 'profile_id', 'profile_image',
+                'category', 'category_name', 'priority_level', 'priority_level_name', 'assignee', 
+                'assignee_name', 'title', 'created_on', 'updated_on', 'status', 'status_name', 
+                'task_detail', 'comments_count',
             ]
